c336a963f8857cc6bb3697260b9d88aac2d64525
Deveesh Shetty <deveeshshetty@gmail.com>
2023-10-08 14:17:20 +0530

    feat: responsive navbar

--- web/client/src/components/navbar.tsx
+++ web/client/src/components/navbar.tsx
@@ -1,29 +1,52 @@
+"use client"
+
 import Image from "next/image"
 import Link from "next/link"
+import { useState } from "react"
+import { FaBars } from "react-icons/fa"
+import { TbX } from "react-icons/tb"
 
 export default function Navbar() {
+  const [toggle, setToggle] = useState(false)
   return (
-    <nav className="grid grid-cols-3 px-4 border-t-8 border-t-foreground/10">
-      <div className="flex items-center h-full place-self-start">
-        <Link href="/">Name</Link>
+    <>
+      <div
+        className="px-4 py-4 md:hidden cursor-pointer bg-foreground/10"
+        onClick={() => setToggle(!toggle)}
+      >
+        {toggle ? <TbX /> : <FaBars />}
       </div>
-      <ul className="relative flex justify-center gap-8 place-self-center bg-foreground/10 w-full h-full py-4 rounded-b-full">
-        <Link href="https://devhost.sosc.org.in" target="_blank">
-          DevHost
-        </Link>
-        <Link href="/hackathon">Hackathon</Link>
-        <Link href="/contact">Contact</Link>
-      </ul>
-      <div className="flex gap-2 items-center h-full place-self-end">
-        <div className="relative w-8 aspect-square">
-          <Image
-            src="https://github.com/heimanpictures.png"
-            alt="User Profile"
-            fill
-            className="rounded-full cursor-pointer"
-          />
+      <nav
+        className={`${
+          toggle
+            ? "translate-y-0 bg-foreground/10"
+            : "-translate-y-[200%] md:translate-y-0"
+        } py-4 md:py-0 md:grid md:grid-cols-6 px-4 md:border-t-8 border-t-foreground/10 transition-all duration-500`}
+      >
+        <div className="flex items-center h-full md:place-self-start">
+          <Link href="/" className="font-bold tracking-wider text-xl">
+            Sankalp
+          </Link>
         </div>
-      </div>
-    </nav>
+        <ul className="relative flex flex-col md:flex-row justify-center gap-2 md:gap-8 col-span-4 place-self-center md:bg-foreground/10 w-full h-full py-4 rounded-b-full">
+          <Link href="https://devhost.sosc.org.in" target="_blank">
+            DevHost
+          </Link>
+          <Link href="/hackathon">Hackathon</Link>
+          <Link href="/contact">Contact</Link>
+        </ul>
+        <div className="flex gap-2 items-center h-full md:place-self-end">
+          <div className="relative w-8 aspect-square">
+            <Image
+              src="https://github.com/heimanpictures.png"
+              alt="User Profile"
+              fill
+              className="rounded-full cursor-pointer"
+            />
+          </div>
+          <p className="md:hidden">Name</p>
+        </div>
+      </nav>
+    </>
   )
 }
