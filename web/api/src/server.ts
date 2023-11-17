aa04b6be50a6401883cc80b5d73796e188b0a296
heimanpictures <heimanpicturesofficial@gmail.com>
2023-11-17 14:30:46 +0530

    API: Request fixes 5

--- web/api/src/server.ts
+++ web/api/src/server.ts
@@ -21,14 +21,14 @@ db.on('open', () => console.log("Mongodb is connected."))
 app.use(express.json())
 
 app.use(cors({
-    origin: process.env.DOMAIN,
+    origin: '*', // process.env.DOMAIN,
     methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
     credentials: true,
     allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization', 'Access-Control-Allow-Origin', 'Access-Control-Allow-Methods', 'Access-Control-Allow-Headers'],
 }));
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', process.env.DOMAIN);
+    res.header('Access-Control-Allow-Origin', '*'); //process.env.DOMAIN);
     res.header('Access-Control-Allow-Methods', 'GET, HEAD, PUT, PATCH, POST, DELETE'); 
     res.header('Access-Control-Allow-Headers', 'Content-Type, Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Origin, Access-Control-Allow-Methods, Access-Control-Allow-Headers'); 
     next();
