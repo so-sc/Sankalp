fb442ca88061ba717f10bc11c806bf727bd46ab9
heimanpictures <heimanpicturesofficial@gmail.com>
2023-11-17 14:27:22 +0530

    API: Request fixes 4

--- web/api/src/server.ts
+++ web/api/src/server.ts
@@ -24,13 +24,13 @@ app.use(cors({
     origin: process.env.DOMAIN,
     methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
     credentials: true,
-    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization', 'Access-Control-Allow-Origin', 'Access-Control-Allow-Methods', 'Access-Control-Allow Headers'],
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization', 'Access-Control-Allow-Origin', 'Access-Control-Allow-Methods', 'Access-Control-Allow-Headers'],
 }));
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', process.env.DOMAIN);
     res.header('Access-Control-Allow-Methods', 'GET, HEAD, PUT, PATCH, POST, DELETE'); 
-    res.header('Access-Control-Allow Headers', 'Content-Type, Origin, X-Requested-With, Content-Type, Accept, Authorization'); 
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Origin, Access-Control-Allow-Methods, Access-Control-Allow-Headers'); 
     next();
 });
 
