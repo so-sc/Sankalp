fbe28dd39c5316092ef510231052d6bf1a0d7583
Deveesh Shetty <deveeshshetty@gmail.com>
2023-12-02 07:09:34 +0530

    adds verify route (finally)

--- web/client/src/components/login.tsx
+++ web/client/src/components/login.tsx
@@ -3,7 +3,8 @@
 import { Button } from "@/components/ui/button"
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-import { H2 } from "@/components/ui/typography"
+import { H1, H2 } from "@/components/ui/typography"
+import { MAIN_EVENT_NAME } from "@/lib/constants"
 import { loginSchema } from "@/lib/schemas"
 import { LoginUser, SignIn } from "@/lib/types"
 import { zodResolver } from "@hookform/resolvers/zod"
@@ -14,9 +15,14 @@ import { useEffect, useState } from "react"
 import { useForm } from "react-hook-form"
 import { TbLoader2 } from "react-icons/tb"
 
-export default function Login() {
+interface LoginProps {
+  isVerify?: boolean
+}
+
+export default function Login({ isVerify }: LoginProps) {
   const router = useRouter()
   const [error, setError] = useState("")
+  const [userIsVerified, setUserIsVerified] = useState(false)
   const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
@@ -24,7 +30,7 @@ export default function Login() {
     if (token) {
       router.push("/dashboard")
     }
-  })
+  }, [router])
 
   const form = useForm<LoginUser>({
     resolver: zodResolver(loginSchema),
@@ -33,6 +39,31 @@ export default function Login() {
       password: "",
     },
   })
+
+  async function verifyUser() {
+    const token = getCookie("token")
+    try {
+      setIsLoading(true)
+      setUserIsVerified(false)
+      setError("")
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/verify`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      const data = await response.json()
+      return data.success
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setIsLoading(false)
+    }
+  }
 
   async function onLogin(values: LoginUser) {
     const signInData: SignIn = {
@@ -59,7 +90,19 @@ export default function Login() {
         setCookie("token", data.token, {
           expires: new Date(Date.now() + 60 * 60 * 24 * 1000),
         })
-        router.push("/dashboard")
+        if (isVerify) {
+          const userVerification = await verifyUser()
+          if (userVerification) {
+            setUserIsVerified(true)
+            setTimeout(() => {
+              router.push("/dashboard")
+            }, 2000)
+          } else {
+            setError("Verification Failed")
+          }
+        } else {
+          router.push("/dashboard")
+        }
       } else {
         setError(data.message)
       }
@@ -74,8 +117,13 @@ export default function Login() {
   return (
     <div className="max-w-3xl mx-auto">
       <div className="mb-10">
+        {isVerify && (
+          <H1 className="lg:text-8xl md:text-6xl text-5xl text-center">
+            {MAIN_EVENT_NAME}
+          </H1>
+        )}
         <p className="text-center mt-5 text-lg md:text-2xl font-bold">
-          Login to Dashboard
+          {isVerify ? "Verify User" : "Login to Dashboard"}
         </p>
       </div>
       <Form {...form}>
@@ -124,23 +172,36 @@ export default function Login() {
             className="mt-4 flex items-center gap-2"
             disabled={isLoading}
           >
-            Login
+            {isVerify ? "Verify" : "Login"}
             {isLoading && <TbLoader2 className="animate-spin" />}
           </Button>
         </form>
         {error && <p className="text-red-500 text-center mt-2">{error}</p>}
       </Form>
       <div>
-        <p className="text-center mt-4">
-          Yet to register?{" "}
-          <Link
-            href={`?${new URLSearchParams({ state: "register" })}`}
-            className="underline underline-offset-2 hover:underline-offset-4"
-          >
-            Register Here
-          </Link>
-        </p>
+        {!isVerify && (
+          <p className="text-center mt-4">
+            Yet to register?{" "}
+            <Link
+              href={`?${new URLSearchParams({ state: "register" })}`}
+              className="underline underline-offset-2 hover:underline-offset-4"
+            >
+              Register Here
+            </Link>
+          </p>
+        )}
       </div>
+      {userIsVerified && (
+        <div className="text-center mt-4">
+          <p className="text-green-500">Verified Succesfully!</p>
+          <p>
+            Go to{" "}
+            <Link href="/dashboard" className="underline">
+              Dashboard
+            </Link>
+          </p>
+        </div>
+      )}
     </div>
   )
 }
