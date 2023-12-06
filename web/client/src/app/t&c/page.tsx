dbd66820c783bccc0e483cc9c71ef6383b85b666
Deveesh Shetty <deveeshshetty@gmail.com>
2023-12-06 23:00:34 +0530

    adds terms and conditions

---
+++ web/client/src/app/t&c/page.tsx
@@ -0,0 +1,9 @@
+export default function TermsAndConditions() {
+  return (
+    <embed
+      src="/Sankalp - T&C.pdf#toolbar=0&navpanes=0&scrollbar=0"
+      type="application/pdf"
+      className="w-full min-h-screen"
+    ></embed>
+  )
+}
