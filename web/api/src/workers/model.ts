9c05ae9bfaa9ebe1f6a3b481a1ae9c1ffbea013a
Akkil M G <heimanpicturesofficial@gmail.com>
2023-12-07 11:29:14 +0530

    API: Slot allotment

--- web/api/src/workers/model.ts
+++ web/api/src/workers/model.ts
@@ -98,7 +98,7 @@ export const HackathonNameModel = {
     name: "Codeblaze",
     date: "8th December 2023",
     venue: "Sahyadri College of Engineering & Management",
-    max: 400,
+    max: 401,
     due: "2023-12-08T08:30:00.234Z"
 }
 
