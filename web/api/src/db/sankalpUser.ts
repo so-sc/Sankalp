85e3c2cb60aced3db0be18547805d81a8147ac6d
Akkil M G <heimanpicturesofficial@gmail.com>
2023-12-07 11:50:16 +0530

    API: Exception handler

--- web/api/src/db/sankalpUser.ts
+++ web/api/src/db/sankalpUser.ts
@@ -671,7 +671,9 @@ export const TalkCount = async () => {
 }
 
 export const EventRegistersVerifyEvent = async (id: string) => {
-    if ((await Event.find({ _id: new mongo.Types.ObjectId(id), verify: true}))[0]) {
+    if ((await Event.find({ _id: id }))[0]) {
+        return { success: false, message: 'Check your qr code.' }
+    } else if ((await Event.find({ _id: new mongo.Types.ObjectId(id), verify: true}))[0]) {
         return { success: false, message: 'Attendee team is already verified.' }
     }else if (await Event.updateOne({ _id: new mongo.Types.ObjectId(id) }, { $set: { verify: true }})) {
         return { success: true }
@@ -682,7 +684,9 @@ export const EventRegistersVerifyEvent = async (id: string) => {
 
 export const EventRegistersVerifyTalk = async (id: string, event: number) => {
     try {
-        if ((await Event.find({ _id: id, 'talk.type.id': event, 'talk.type.verify': true}))[0]) {
+        if ((await Event.find({ _id: id }))[0]) {
+            return { success: false, message: 'Check your qr code.' }
+        } else if ((await Event.find({ _id: id, 'talk.type.id': event, 'talk.type.verify': true}))[0]) {
             return { success: false, message: 'Attendee is already verified.' }
         }else if (await Event.findOne(
             { _id: id, 'talk.type.id': event },
@@ -1063,7 +1067,9 @@ export const HackathonSendEmailAll = async (data: any) => {
 
 
 export const hackathonRegistersVerify = async (id: string) => {
-    if ((await Hackathon.find({ _id: new mongo.Types.ObjectId(id), verify: true}))[0]) {
+    if ((await Hackathon.find({ _id: id }))[0]) {
+        return { success: false, message: 'Check your qr code.' }
+    } if ((await Hackathon.find({ _id: new mongo.Types.ObjectId(id), verify: true}))[0]) {
         return { success: false, message: 'Hackathon team is already verified.' }
     }else if (await Hackathon.updateOne({ _id: new mongo.Types.ObjectId(id) }, { $set: { verify: true }})) {
         return { success: true }
